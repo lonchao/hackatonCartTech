49c92de17b54121be8b63b8bb08c113037bfa752
lon way chao <lonwaychao@gmail.com>
2019-11-29 22:33:01 -0300

    reader controller

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 
+const readerController = require('./controllers/ReaderController');
+
 app.get('/', function(req, res) {
   res.send('CartTech 0.1!');
+});
+
+app.get('/read', function(req, res) {
+  return readerController.read(req, res);
 });
 
 app.listen(3000, function() {
